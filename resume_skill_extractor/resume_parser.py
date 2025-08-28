462e07a4e10010fa72cd173c4363f730638e73cf
sneha Wayadande <snehawaydande2000@gmail.com>
2025-08-28 15:49:28 +0530

    requirement.txt updated

--- resume_skill_extractor/resume_parser.py
+++ resume_skill_extractor/resume_parser.py
@@ -6,9 +6,16 @@ import requests
 import json
 import streamlit as st
 
-# Load spaCy (optional, can skip for speed)
-nlp = spacy.load("en_core_web_sm")
 
+import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
+    
 # --- PDF Extraction ---
 @st.cache_data
 def extract_text_from_pdf_cached(file_bytes):
